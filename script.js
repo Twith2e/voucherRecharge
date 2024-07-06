1c09ad4217bf43ba006a890ebbde749721fc7ee3
twith2e <abdulbasitadebajo1619@gmail.com>
2024-07-06 20:33:42 +0100

    functionality change

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-let balanceDb = [{ provider: "starlink", amount: 0 }];
+let balanceDb = [
+  { provider: "mtn", amount: 0 },
+  { provider: "9mobile", amount: 0 },
+  { provider: "glo", amount: 0 },
+  { provider: "airtel", amount: 0 },
+];
 let usedVoucher = [];
+const pinRegex = /^\*311\*\d{10}#$/;
+
 function inputAmount(button) {
   amount.value = button.value;
 }
@@ -10,7 +17,6 @@ function generate() {
       let random = Math.floor(Math.random() * 10);
       pin += random;
     }
-    console.log(pin.length);
     ourPin.innerText = pin;
     return;
   }
@@ -18,50 +24,44 @@ function generate() {
 }
 
 function load() {
-  let isProivder = false;
-  let providerAmount = Number(amount.value);
+  let providerIndex = 0;
+  let newBalance = 0;
   if (
-    voucherInput.value.startsWith("*311*") &&
-    voucherInput.value.endsWith("#") &&
-    voucherInput.value.includes(ourPin.innerText) &&
-    voucherInput.value.length === 16
+    pinRegex.test(voucherInput.value) &&
+    voucherInput.value.includes(ourPin.innerText)
   ) {
-    balanceDb.forEach((db, index) => {
-      if (balanceDb[index].provider === provider.value) {
-        balanceDb[index].amount += Number(amount.value);
-        providerAmount = balanceDb[index].amount;
-        isProivder = true;
+    balanceDb.forEach((match, index) => {
+      if (provider.value === match.provider) {
+        providerIndex = index;
+        return;
       }
     });
-  } else {
-    alert("Check your pin again");
-    return;
-  }
-  if (isProivder) {
-    if (usedVoucher.includes(ourPin.innerText)) {
-      alert("Voucher already used!");
-      return;
+    let found = usedVoucher.find((match) => match === ourPin.innerText);
+    if (found === ourPin.innerText) {
+      alert("Voucher is already used");
+    } else {
+      balanceDb[providerIndex].amount += Number(amount.value);
+
+      successMessage.style.visibility = "visible";
+      providerHeader.innerText = balanceDb[providerIndex].provider;
+      amountRecharged.innerText = amount.value;
+      balance.innerText = balanceDb[providerIndex].amount;
+      successMessage.style.color = "white";
+      if (provider.value === "mtn") {
+        successMessage.style.backgroundColor = "yellow";
+        successMessage.style.color = "brown";
+      } else if (provider.value === "9mobile") {
+        successMessage.style.backgroundColor = "yellowGreen";
+      } else if (provider.value === "airtel") {
+        successMessage.style.backgroundColor = "red";
+      } else {
+        successMessage.style.backgroundColor = "green";
+      }
+      usedVoucher.push(ourPin.innerText);
     }
-    providerHeader.innerText = `${provider.value}!`;
-    amountRecharged.innerText = amount.value;
-    successMessage.style.visibility = "visible";
-    balance.innerText = `₦${providerAmount}`;
   } else {
-    if (usedVoucher.includes(ourPin.innerText)) {
-      alert("Voucher already used!");
-      return;
-    }
-    providerHeader.innerText = `${provider.value}!`;
-    amountRecharged.innerText = amount.value;
-    successMessage.style.visibility = "visible";
-    balance.innerText = `₦${providerAmount}`;
-    balanceDb.push({
-      provider: provider.value,
-      amount: Number(amount.value),
-    });
+    alert("Invalid input!");
   }
-  usedVoucher.push(ourPin.innerText.trim());
-  console.log(balanceDb);
 }
 
 // function load() {
